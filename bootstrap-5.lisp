8e868632ffd5d13e2d0ebf4b414a785515e9d702
Juan Reynoso Elias <juan@ikkiware.com>
2022-05-13 17:29:25 -0500

    Add Bootstrap 5.2.0 beta

--- bootstrap-5.lisp
+++ bootstrap-5.lisp
@@ -30,7 +30,7 @@
 		      :content "width=device-width, initial-scale=1")
 	      (<:link :rel "stylesheet"
 		      :type "text/css"
-		      :href "https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css")
+		      :href "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css")
 	      (<:link :rel "stylesheet"
 		      :type "text/css"
 		      :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css")
@@ -51,7 +51,7 @@
 				:src "/img/lisp-alien.png"))
 		  (<:div :class "col-12 pt-4"
 			 (<:h1 :class "text-primary"
-			       "Bootstrap 5.1.2")
+			       "Bootstrap 5.2.0 beta")
 			 (<:h3 :class "text-muted"
 			      "Build fast, responsive sites with Bootstrap."))
 		  (<:div :class "col-12 col-lg-6 pb-2"
