7e8123e200bb254e23b3f267e6314d0bfbaf420a
Juan Reynoso Elias <juan@ikkiware.com>
2022-05-13 17:30:30 -0500

    Add Bootstrap Icons v1.8.2

--- bootstrap-5.lisp
+++ bootstrap-5.lisp
@@ -33,7 +33,7 @@
 		      :href "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css")
 	      (<:link :rel "stylesheet"
 		      :type "text/css"
-		      :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css")
+		      :href "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css")
 	      (<:script :type "text/javascript"
 			:src "https://code.jquery.com/jquery-3.6.0.min.js")
 	      (<:title (<:as-html ,title)))
